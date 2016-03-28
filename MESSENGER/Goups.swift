4a3e11c8d1f5de9b1e82890f80aaaed50f754536
Kayamba Mukanzu <kmukanzu@icloud.com>
2016-03-28 16:34:05 -0500

    Added Options alertAction to Groups view controller.

--- MESSENGER/Goups.swift
+++ MESSENGER/Goups.swift
@@ -10,6 +10,27 @@ import Foundation
 
 class Groups : UITableViewController {
     
+    @IBAction func Options(sender: AnyObject) {
+        
+        let actionAlert = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+        
+        let searchGroup = UIAlertAction(title: "Search Groups", style: .Default) { (Alert:UIAlertAction) -> Void in
+            print("Search Groups button was pressed")
+        }
+        let createGroup = UIAlertAction(title: "Create Group", style: .Default) { (Alert:UIAlertAction) -> Void in
+            print("Create Group button was pressed")
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel) { (Alert:UIAlertAction) -> Void in
+            print("Cancel")
+        }
+        actionAlert.addAction(searchGroup)
+        actionAlert.addAction(createGroup)
+        actionAlert.addAction(cancel)
+        
+        self.presentViewController(actionAlert, animated: true, completion: nil)
+        
+    }
+
     @IBOutlet var Open: UIBarButtonItem!
     
     override func viewDidLoad() {
